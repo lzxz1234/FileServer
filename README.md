72435a784e4f618daf58b31f0a70c59be825226b
lzxz1234 <1qaz@WSX>
2014-12-04 18:55:23 +0800

    修改说明文件
    [ADD] 添加项目说明
    [MOD]修改 FSHttpClient 依赖 FSClient 实现

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
-FileServer
-==========
+# FileServer #
 
-简易文件用品
+简易文件服务器，提供基本的存、取、删操作，并对相同文件的多次存放做合并处理，节省磁盘空间，基于 nfs-rpc 实现传输协议。
+
+## 核心类 ##
+
+- **FSServer**: 服务器启动类，依赖 Tomcat 启动时额外提供 HTTP 访问接口。
+- **FSClient**: 全功能客户端类，提供全部存、取、删操作。
+- **FSHttpClient**: HTTP 专用存文件接口，FSClient 的浅封装。
+
+## 配置文件 ##
+
+fileserver.properties：
+
+    server.port=8081 --监听端口，NFS-RPC 监听，和 Tomcat 端口无关
+    server.threads=10 --服务器处理线程数
+
+	fs.root=e://fs/ --文件存储目录
+	fs.berkeleydb.root=e://fs/berkeley  --BerkeleyDB 存储位置，主要用于文件摘要到文件路径的文件数目的映射
+	fs.berkeleydb.sha2file.name=STORE.SHA.TO.FILE  --不用修改
+	fs.berkeleydb.sha2count.name=STORE.SHA.TO.COUNT  --不用修改
+
+## 使用样例 ##
+
+可以参考 `FSHttpClient` 实现。
+
+## 扩展 ##
+
+### 关于缓存 ###
+
+启动服务器时会初始化工作链，目前工作链仅两环：`ExistsFileWorker` 和 `BerkeleyBasedFileSystemWorker`，第一环决定文件是否存在及已经存在时的处理逻辑，第二环才会在磁盘上写入目标文件，需要缓存时另行添加缓存层。
+
+### 关于 HTTP 下载接口 ###
+
+目前下载接口是 `download.jsp` 提供的，它也是 `FSClient` 的浅封装，其中缺少 MIME 信息等内容，需要时在此处扩展。
--- src/com/chn/fs/FSHttpClient.java
+++ src/com/chn/fs/FSHttpClient.java
@@ -9,17 +9,6 @@
  */
 package com.chn.fs;
 
-import java.io.WriteAbortedException;
-import java.net.ConnectException;
-import java.net.SocketException;
-
-import code.google.nfs.rpc.Codecs;
-import code.google.nfs.rpc.client.Client;
-import code.google.nfs.rpc.netty4.client.Netty4ClientFactory;
-import code.google.nfs.rpc.protocol.SimpleProcessorProtocol;
-
-import com.chn.fs.message.ExistRequest;
-import com.chn.fs.message.WriteRequest;
 import com.chn.fs.util.MD;
 
 /**
@@ -30,19 +19,9 @@ import com.chn.fs.util.MD;
  */
 public class FSHttpClient implements ConstantCode {
 
-    private static final int DEFAULT_CALL_TIMEOUT       = 30000;
-    private static final int DEFAULT_CLIENT_NUMS        = 100;
-    private static final int DEFAULT_CONNECT_TIMEOUT    = 10000;
-    
+    private FSClient client;
     private String serverIp;
-    private int rpcPort;
     private int httpPort;
-    private int callTimeout = DEFAULT_CALL_TIMEOUT;
-    private int clientNums = DEFAULT_CLIENT_NUMS;
-    private int connectTimeout = DEFAULT_CONNECT_TIMEOUT;
-    
-    private Client client;
-    
     
     /**
      * 构造方法。
@@ -52,95 +31,15 @@ public class FSHttpClient implements ConstantCode {
      */
     public FSHttpClient(String serverIp, int rpcPort, int httpPort) {
         
+        this.client = new FSClient(serverIp, rpcPort);
         this.serverIp = serverIp;
-        this.rpcPort = rpcPort;
         this.httpPort = httpPort;
     }
     
     public String add(byte[] bytes) throws Exception {
         
-        byte[] digest = MD.digest("SHA-256", bytes);
-        
-        ExistRequest existReq = new ExistRequest(digest, 1);
-        String existResp = (String) this.call(existReq);
-        if (RESPONSE_CODE_FALSE.equals(existResp)) {
-            WriteRequest writeReq = new WriteRequest(digest, bytes, 1);
-            String writeResp = (String) this.call(writeReq);
-            if (RESPONSE_CODE_FALSE.equals(writeResp)) {
-                throw new WriteAbortedException("写入文件失败！", new Exception());
-            } 
-        }
+        byte[] digest = client.write(bytes, 1);
         return "http://"+serverIp+":"+httpPort+"/download.jsp?id="+MD.getHexString(digest);
-    }
-    
-    /*
-     * 调用RPC。
-     * 
-     * @param   request                 请求对象
-     * 
-     * @return  响应对象
-     * 
-     * @throws  ConnectException        当RPC连接错误时抛出
-     * @throws  SocketException         当RPC调用出现通讯故障时抛出
-     * @throws  InterruptedException    当RPC调用被中断时抛出
-     */
-    private Object call(Object request) throws ConnectException, SocketException, 
-        InterruptedException {
-        
-        if (request == null) throw new IllegalArgumentException("RPC请求参数不能为null！");
-        
-        if (!this.isConnected()) throw new ConnectException("RPC连接错误！");
-        
-        Object resp = null;
-        boolean retry = false;
-        do {
-            try {
-                resp = this.client.invokeSync(request, this.callTimeout, Codecs.JAVA_CODEC, 
-                                              SimpleProcessorProtocol.TYPE);
-                retry = false;
-            } catch (Exception e) {
-                String errMsg = e.getMessage();
-                if ((errMsg != null) && errMsg.startsWith("receive response timeout")) {
-                    retry = true;
-                } else if ((errMsg != null) && errMsg.equals("Get response error")) {
-                    throw new InterruptedException("RPC调用被中断！");
-                } else {
-                    this.closeClient();
-                    throw new SocketException("RPC调用通讯错误！");
-                }
-            }
-        } while (retry);
-        
-        return resp;
-    }
-    
-    /*
-     * 判断是否连接到RPC服务器，如果没有连接则尝试连接。
-     * 
-     * @return  是否连接到RPC服务器。
-     */
-    private boolean isConnected() {
-         
-        if (this.client == null) {
-            try {
-                this.client = Netty4ClientFactory.getInstance().get(this.serverIp, 
-                        this.rpcPort, this.connectTimeout, this.clientNums);
-                return true;
-            } catch (Exception e) {
-                throw new RuntimeException("连接失败", e);
-            }
-        } else {
-            return true;
-        }
-    }
-    
-    /*
-     * 关闭客户端连接。
-     */
-    private void closeClient() {
-        
-        Netty4ClientFactory.getInstance().removeClient(this.serverIp + ":" + this.rpcPort, this.client);
-        this.client = null;
     }
     
 }
